d04d1ab792d1d42ee363d133927912270f4e66b0
Jose Prieto <joseprieto9657@gmail.com>
2022-07-04 23:08:43 +0200

    DFS and topological sorting implemented

--- src/assignment2/Digraph.java
+++ src/assignment2/Digraph.java
@@ -1,68 +1,79 @@
 package assignment2;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 // Build the graph receiving a matrix or an adjList,
 // but my graph is going to be represented by an adjList
 // to be more efficient.
 
-// I am going to use DFS to solve the problem of detect
-// if the graph has cycles
+// TODO explain how I resolve the problem and the cost
 public class Digraph {
 
     private int V;
-    private int A;
     private List<List<Integer>> adjList;
 
+    private boolean isCyclic;
+
+    // Build digraph with adjacency matrix
     public Digraph(boolean[][] adjMatrix) {
         this.V = adjMatrix.length;
-        this.A = 0;
         adjList = new ArrayList<>();
         for(int i = 0; i < adjMatrix.length; i++) {
             List<Integer> auxList = new ArrayList<>();
             for(int j = 0; j < adjMatrix[i].length; j++) {
-                if(adjMatrix[i][j])
+                if(i != j && adjMatrix[i][j])
                     auxList.add(j);
             }
             adjList.add(auxList);
         }
     }
 
+    // Build digraph with adjacency list
     public Digraph(List<List<Integer>> adjList) {
         this.V = adjList.size();
-        this.A = 0;
         this.adjList = adjList;
     }
 
-    private void dfs(int v, boolean[] visited) {
+    // DFS search adapted to know if the graph is cyclic
+    private void dfs(int v, boolean[] visited, boolean[] recursion, LinkedList<Integer> solution) {
         visited[v] = true;
-        System.out.print(V + " ");
+        recursion[v] = true;
 
-        Iterator<Integer> i = adjList.get(v).listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n])
-                dfs(n, visited);
-        }
-    }
-
-    public void dfs(int v) {
-        boolean visited[] = new boolean[V];
-        dfs(v, visited);
-    }
-
-    public void print() {
-        for(int v = 0; v < V; v++) {
-            System.out.print(v + ": ");
-            for(Integer w : adjList.get(v)){
-                System.out.print(w + " ");
-                ++A;
+        List<Integer> children = adjList.get(v);
+        for (Integer w : children) {
+            if(isCyclic) return;
+            if(!visited[w]) {
+                dfs(w, visited, recursion, solution);
             }
-            System.out.println();
+            else if(recursion[w]) {
+                isCyclic = true;
+            }
         }
-        System.out.println(V + " vertexes, " + A + " edges");
+
+        solution.addLast(v);
+        recursion[v] = false;
+    }
+
+    public void printSolution() {
+        boolean visited[] = new boolean[V];
+        boolean recursion[] = new boolean[V];
+        LinkedList<Integer> solution = new LinkedList<>();
+
+        for(int v = 0; v < V; v++) {
+            if(!visited[v] && !isCyclic) {
+                dfs(v, visited, recursion, solution);
+            }
+        }
+
+        if(isCyclic) {
+            System.out.println("Cycled detected");
+        }
+        else {
+            for(Integer v : solution) {
+                System.out.print(v + " ");
+            }
+        }
+
     }
 
 }
--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -1,11 +1,22 @@
 package assignment2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    // Adj Matrix
+    private static boolean[][] matrix = new boolean[7][7];
+
+    // Adj List
+    private static List<List<Integer>> adjList = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        System.out.println("---------------------- Input 1 ----------------------");
+
         // Input 1 (matrix)
-        boolean[][] matrix = new boolean[7][7];
+        matrix = new boolean[7][7];
         matrix[0][6] = true;
         matrix[1][0] = true;
         matrix[2][1] = true;
@@ -14,15 +25,87 @@ public class Main {
         matrix[5][3] = true;
         matrix[6][0] = true;
 
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("-----> Adjacency matrix as the input ");
+        System.out.print("Expected output: ");
+        Digraph sol1A = new Digraph(matrix);
+        sol1A.printSolution();
 
-        Digraph digraph = new Digraph(matrix);
-        digraph.print();
+        System.out.println();
+
+        // Input 1 (list)
+        adjList = new ArrayList<>();
+        List<Integer> auxList = new ArrayList<>();
+        auxList.add(1);
+        auxList.add(6);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(2);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(3);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(4);
+        auxList.add(5);
+        adjList.add(auxList);
+        adjList.add(new ArrayList<>());
+        adjList.add(new ArrayList<>());
+        auxList = new ArrayList<>();
+        auxList.add(0);
+        adjList.add(auxList);
+
+        System.out.println("-----> Adjacency list as the input");
+        System.out.print("Expected output: ");
+        Digraph sol1B = new Digraph(adjList);
+        sol1B.printSolution();
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("---------------------- Input 2 ----------------------");
+
+        // Input 2 (matrix)
+        matrix = new boolean[7][7];
+        matrix[0][6] = true;
+        matrix[1][0] = true;
+        matrix[2][1] = true;
+        matrix[3][2] = true;
+        matrix[4][3] = true;
+        matrix[5][3] = true;
+
+        System.out.println("-----> Adjacency matrix as the input");
+        System.out.print("Expected output: ");
+        Digraph sol2A = new Digraph(matrix);
+        sol2A.printSolution();
+
+        System.out.println();
+
+        // Input 2 (list)
+        adjList = new ArrayList<>();
+        auxList = new ArrayList<>();
+        auxList.add(1);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(2);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(3);
+        adjList.add(auxList);
+        auxList = new ArrayList<>();
+        auxList.add(4);
+        auxList.add(5);
+        adjList.add(auxList);
+        adjList.add(new ArrayList<>());
+        adjList.add(new ArrayList<>());
+        auxList = new ArrayList<>();
+        auxList.add(0);
+        adjList.add(auxList);
+
+        System.out.println("-----> Adjacency list as the input");
+        System.out.print("Expected output: ");
+        Digraph sol2B = new Digraph(adjList);
+        sol2B.printSolution();
+
     }
 
 }
